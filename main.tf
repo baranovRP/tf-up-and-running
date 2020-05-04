278d7fc18928137283c451d480cf03a7bafc390d
baranovrp <baranov.r.p@gmail.com>
2020-05-04 19:38:28 +0200

    Deploy web server

--- main.tf
+++ main.tf
@@ -1,13 +1,45 @@
 ###
 provider "aws" {
-  region = "eu-west-2"
+  region  = "eu-west-2"
+  profile = "ora2postgres"
+  version = "~> 2.0"
 }
 
-resource "aws_instance" "tf_ubuntu" {
-  ami = "ami-0917237b4e71c5759"
-  instance_type = "t2.micro"
+resource "aws_instance" "tf_ami" {
+  ami                    = "ami-01a6e31ac994bbc09"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_dmz.id]
 
-tags = {
-  Name = "tf-WebServer"
+  user_data = file("user_data.sh")
+
+  tags = {
+    Name = "tf-WebServer"
+  }
 }
+
+resource "aws_security_group" "web_dmz" {
+  name = "tf-web-dmz"
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }

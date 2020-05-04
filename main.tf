f8261ed2a9715e23a6520339e5c5f0637f19a363
baranovrp <baranov.r.p@gmail.com>
2020-05-04 21:05:47 +0200

    Deploy load balancer

--- main.tf
+++ main.tf
@@ -26,8 +26,11 @@ resource "aws_launch_configuration" "tf_ami" {
 }
 
 resource "aws_autoscaling_group" "web_cluster" {
-  launch_configuration   = aws_launch_configuration.tf_ami.id
-  vpc_zone_identifier    = data.aws_subnet_ids.default.ids
+  launch_configuration = aws_launch_configuration.tf_ami.id
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -43,25 +46,100 @@ resource "aws_security_group" "web_dmz" {
   name = "tf-web-dmz"
 
   ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    from_port        = var.server_port
+    to_port          = var.server_port
+    protocol         = var.protocols.tcp
+    cidr_blocks      = [var.cidrblocks.cidrblock_all_ipv4]
+    ipv6_cidr_blocks = [var.cidrblocks.cidrblock_all_ipv6]
   }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = [var.cidrblocks.cidrblock_all_ipv4]
+    ipv6_cidr_blocks = [var.cidrblocks.cidrblock_all_ipv6]
   }
 
   ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = var.protocols.tcp
+    cidr_blocks = [var.cidrblocks.cidrblock_all_ipv4]
+  }
+}
+
+resource "aws_lb" "tf_balancer" {
+  name               = "tf-lb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnet_ids.default.ids
+  security_groups    = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.tf_balancer.arn
+  port              = var.server_port
+  protocol          = var.protocols.http
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = "404"
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "tf-alb"
+
+  ingress {
+    from_port        = var.server_port
+    to_port          = var.server_port
+    protocol         = var.protocols.tcp
+    cidr_blocks      = [var.cidrblocks.cidrblock_all_ipv4]
+    ipv6_cidr_blocks = [var.cidrblocks.cidrblock_all_ipv6]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = -1
+    cidr_blocks      = [var.cidrblocks.cidrblock_all_ipv4]
+    ipv6_cidr_blocks = [var.cidrblocks.cidrblock_all_ipv6]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name     = "tf-asg"
+  port     = var.server_port
+  protocol = var.protocols.http
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = var.protocols.http
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    field  = "path-pattern"
+    values = ["*"]
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
   }
 }
--- output.variables.tf
+++ output.variables.tf
@@ -1,6 +1,4 @@
-/*
-output "public_ip" {
-  value       = aws_instance.tf_ami.public_ip
-  description = "The public IP address of the web server"
+output "alb_dns_name" {
+  value       = aws_lb.tf_balancer.dns_name
+  description = "The domain name of the load balancer"
 }
-*/

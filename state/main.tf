9e5c2ba9311cdf9b3ff03f14237835da0e3b19ff
baranovrp <baranov.r.p@gmail.com>
2020-05-05 18:59:19 +0200

    Save state remotely

---
+++ state/main.tf
@@ -0,0 +1,48 @@
+###
+provider "aws" {
+  version    = "~> 2.0"
+  region     = "eu-west-2"
+  shared_credentials_file = "~/.aws/credentials"
+  profile    = "ora2postgres"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "tf-state-eu-west-2-rnbv"
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "tf_locks" {
+  name         = "tf-locks-eu-west-2-rnbv"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tf-state-eu-west-2-rnbv"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
+
+    dynamodb_table = "tf-locks-eu-west-2-rnbv"
+    encrypt        = true
+  }
+}
